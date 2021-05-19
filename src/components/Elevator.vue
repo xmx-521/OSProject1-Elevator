bbce4acb9c16c57f1624f6251a1b38657c075bc6
徐满心 <1426356297@qq.com>
2021-05-19 10:25:27 +0800

    添加基本内部调度逻辑

--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-green-50 shadow-2xl elevator left">
+    <div class="bg-green-50 shadow-2xl elevatorInside left">
         <div class="elevatorBoarderLeft left">
             <div v-for="count in this.floorNum" :key="count" class="floorNumber">{{this.floorNum+1-count}}</div>
         </div>
@@ -12,7 +12,7 @@
         <div class="elevatorBoarderRight right">
             <div v-for="count in this.floorNum/2" :key="count">
                 <el-button v-for="count2 in 2" :key="count2" type="primary" size="mini" class="bg-green-50 black-font"
-                    circle @click="changeFloor(this.floorNum+count2-count*2)">{{this.floorNum+count2-count*2}}
+                    circle @click="selectFloor(this.floorNum+count2-count*2)">{{this.floorNum+count2-count*2}}
                 </el-button>
             </div>
             <div>
@@ -29,43 +29,121 @@
     export default {
         name: 'Elevator',
         props: {
-            floorNum: Number
+            floorNum: {
+                type: Number,
+                default: 20
+            }
         },
         data() {
             return {
                 currentFloor: 1,
                 goalFloor: 1,
-                topGap: "570px"
+                topGapNumber: 570,
+                selectedFloor: [...Array(20)].map(() => false)
             }
         },
+        computed: {
+            moveDirection() {
+                if (this.currentFloor === this.goalFloor || this.goalFloor == -1) {
+                    return 0;
+                } else if (this.currentFloor > this.goalFloor) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            },
+            isBusy() {
+                let allUnSelected = true;
+                for (let i = 0; i < this.floorNum; i++) {
+                    if (this.selectedFloor[i] === true) {
+                        allUnSelected = false;
+                        break;
+                    }
+                }
+                return !allUnSelected;
+            },
+            topGap() {
+                return `${this.topGapNumber}px`
+            }
+        },
+        mounted() {
+            // console.log(this.moveDirection);
+            // console.log(this.selectedFloor);
+        },
         methods: {
-            moveFloor() {
+            selectFloor(floor) {
+                if (this.isBusy === false) {
+                    this.selectedFloor[floor - 1] = true;
+                    this.run();
+                } else {
+                    this.selectedFloor[floor - 1] = true;
+                }
+            },
+            getNextGoal() {
+                if (this.isBusy === false) { // Elevator is free.
+                    return -1; // bogus.
+                } else if (this.moveDirection === 0) { // Elevator is not free but stable.
+                    for (let i = 0; i < this.floorNum; i++) {
+                        if (this.selectedFloor[i] === true) {
+                            return i + 1;
+                        }
+                    }
+                } else if (this.moveDirection === 1) { // Elevator is moving up.
+                    for (let i = this.currentFloor - 1; i < this.floorNum; i++) {
+                        if (this.selectedFloor[i] === true) {
+                            return i + 1;
+                        }
+                    }
+                } else { // Elevator is moving down.
+                    for (let i = 0; i < this.currentFloor; i++) {
+                        if (this.selectedFloor[i] === true) {
+                            return i + 1;
+                        }
+                    }
+                }
+            },
+            run() {
+                this.goalFloor = this.getNextGoal();
                 let start = Date.now();
-                let changeFloor = this.goalFloor - this.currentFloor;
                 let vueTimer = setInterval(() => {
                     let timePassed = Date.now() - start;
-                    if (timePassed >= Math.abs(changeFloor) * 500) {
+                    if ((this.moveDirection === 1 && this.topGapNumber <= this.getTopGap(this.goalFloor)) || (
+                            this
+                            .moveDirection === -1 && this.topGapNumber >= this.getTopGap(this.goalFloor))) {
                         clearInterval(vueTimer);
-                        this.currentFloor = this.goalFloor;
-                        return;
+                        setTimeout(() => {
+                            this.selectedFloor[this.goalFloor - 1] = false
+                            this.updateCurrentAndGoal()
+                            if (this.isBusy === false) {
+                                return;
+                            } else {
+                                this.run()
+                            }
+                            return;
+                        }, 2000)
                     }
-                    this.topGap = `${((this.floorNum - this.currentFloor) - changeFloor * (timePassed / (Math.abs(
-                        changeFloor) * 500))) * 30}px`
+                    this.topGapNumber = this.getTopGap(this.currentFloor) - this.moveDirection * timePassed /
+                        10;
                 }, 20)
             },
             changeFloor(floorNum) {
                 this.goalFloor = floorNum;
-                this.moveFloor()
+                this.moveFloor();
             },
-            testMethod() {
-                this.topGap = 300;
+            getTopGap(floor) {
+                return 600 - floor * 30
+            },
+            updateCurrentAndGoal() {
+                let tempFloor = this.goalFloor;
+                this.goalFloor = this.getNextGoal();
+                this.currentFloor = tempFloor;
             }
         }
     }
 </script>
 
 <style>
-    .elevator {
+    .elevatorInside {
         box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
         border: 1px solid pink;
         width: 230px;
