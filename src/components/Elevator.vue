fc496b2f75f1d6ba0c2ef7187dda3e62911e09e5
徐满心 <1426356297@qq.com>
2021-05-19 18:08:24 +0800

    内控算法完成

--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="bg-green-50 shadow-2xl elevatorInside left">
+    <div></div>
+    <div class="bg-gray-100 shadow-2xl elevatorInside left">
         <div class="elevatorBoarderLeft left">
             <div v-for="count in this.floorNum" :key="count" class="floorNumber">{{this.floorNum+1-count}}</div>
         </div>
-        <transition>
-            <div class="floor left">
-                <div class="door left"></div>
-                <div class="door right"></div>
-            </div>
-        </transition>
+        <div class="floor left bg-gray-400">
+            <div class="door left left-door"></div>
+            <div class="door right right-door"></div>
+        </div>
         <div class="elevatorBoarderRight right">
             <div v-for="count in this.floorNum/2" :key="count">
-                <el-button v-for="count2 in 2" :key="count2" size="mini" class="bg-green-50 black-font" circle
-                    @click="selectFloor(this.floorNum+count2-count*2)">{{this.floorNum+count2-count*2}}
+                <el-button v-for="count2 in 2" :key="count2" size="mini" type="primary"
+                    :class="buttonBackground[this.floorNum+count2-count*2-1]" circle
+                    @click="selectFloor(this.floorNum+count2-count*2,$event)">{{this.floorNum+count2-count*2}}
                 </el-button>
             </div>
             <div>
@@ -38,8 +38,10 @@
             return {
                 currentFloor: 1,
                 topGapNumber: 570,
+                doorGapNumber: 18.5,
                 selectedFloor: [...Array(20)].map(() => false),
-                moveDirection: 1
+                moveDirection: 1,
+                buttonBackground: [...Array(20)].map(() => "white")
             }
         },
         computed: {
@@ -55,6 +57,9 @@
             },
             topGap() {
                 return `${this.topGapNumber}px`
+            },
+            doorGap() {
+                return `${this.doorGapNumber}px`
             }
         },
         mounted() {
@@ -62,14 +67,19 @@
             // console.log(this.selectedFloor);
         },
         methods: {
-            selectFloor(floor) {
+            selectFloor(floor, event) {
+                console.log(this.isBusy)
+                this.clickHandler(event)
                 if (this.isBusy === false) {
                     this.selectedFloor[floor - 1] = true;
+                    this.buttonBackground[floor - 1] = "blue";
                     this.moveOneFloor();
                 } else {
                     this.selectedFloor[floor - 1] = true;
+                    this.buttonBackground[floor - 1] = "blue";
                 }
             },
+
             moveOneFloor() {
                 this.computeMoveDirection()
                 if (this.selectedFloor[this.currentFloor - 1] === true) {
@@ -129,14 +139,45 @@
 
             openTheDoor() {
                 setTimeout(() => {
-                    this.selectedFloor[this.currentFloor - 1] = false
-                    if (this.isBusy === false) {
-                        return;
-                    } else {
-                        this.moveOneFloor()
-                    }
-                    return;
+                    let vueTimer = setInterval(() => {
+                        if (this.doorGapNumber <= 1) {
+                            clearInterval(vueTimer);
+                            this.closeTheDoor();
+                        }
+
+                        // Update floor position.
+                        this.doorGapNumber = this.doorGapNumber - 0.5;
+                        // console.log(this.moveDirection)
+                        // console.log(this.topGapNumber)
+                    }, 20)
+                }, 500)
+            },
+
+            closeTheDoor() {
+                setTimeout(() => {
+                    let vueTimer = setInterval(() => {
+                        if (this.doorGapNumber >= 18.5) {
+                            clearInterval(vueTimer);
+                            this.selectedFloor[this.currentFloor - 1] = false
+                            this.buttonBackground[this.currentFloor - 1] = "white"
+                            if (this.isBusy === false) {
+                                return;
+                            } else {
+                                this.moveOneFloor();
+                            }
+                        }
+                        this.doorGapNumber = this.doorGapNumber + 0.5;
+                    }, 20)
                 }, 2000)
+            },
+
+
+            clickHandler(evt) {
+                let target = evt.target;
+                if (target.nodeName == "SPAN") {
+                    target = evt.target.parentNode;
+                }
+                target.blur();
             }
         }
     }
@@ -145,7 +186,7 @@
 <style>
     .elevatorInside {
         box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
-        border: 1px solid pink;
+        border: 2px solid black;
         width: 230px;
         height: 600px;
         margin-right: 50px;
@@ -153,21 +194,27 @@
     }
 
     .elevatorBoarderLeft {
-        --tw-bg-opacity: 1;
-        background-color: rgba(252, 231, 243, var(--tw-bg-opacity));
+        background-image: linear-gradient(to right, var(--tw-gradient-stops));
+        --tw-gradient-from: #000;
+        --tw-gradient-stops: var(--tw-gradient-from), var(--tw-gradient-to, rgba(0, 0, 0, 0));
+        --tw-gradient-to: #1f2937;
         width: 30px;
         height: 600px;
     }
 
     .elevatorBoarderRight {
-        --tw-bg-opacity: 1;
-        background-color: rgba(252, 231, 243, var(--tw-bg-opacity));
+        background-image: linear-gradient(to right, var(--tw-gradient-stops));
+        --tw-gradient-from: #111827;
+        --tw-gradient-stops: var(--tw-gradient-from), var(--tw-gradient-to, rgba(17, 24, 39, 0));
+        --tw-gradient-stops: var(--tw-gradient-from), #374151, var(--tw-gradient-to, rgba(55, 65, 81, 0));
+        --tw-gradient-to: #9ca3af;
         width: 120px;
         height: 600px;
     }
 
     .blue {
         background-color: #409EFF;
+        color: white;
     }
 
     .left {
@@ -191,41 +238,62 @@
     }
 
     .door {
-        --tw-bg-opacity: 1;
-        background-color: rgba(165, 180, 252, var(--tw-bg-opacity));
-        width: 20px;
-        height: 30px;
+        background-color: black;
+        width: 19px;
+        height: 26px;
     }
 
     .floor {
-        --tw-bg-opacity: 1;
-        background-color: rgba(224, 231, 255, var(--tw-bg-opacity));
-        width: 80px;
-        height: 30px;
+        width: 76px;
+        height: 26px;
         margin-top: v-bind(topGap);
+        border: 2px solid black;
     }
 
     .floorNumber {
         width: 30px;
         height: 30px;
-        color: #606266;
+        color: white;
         font-size: 16px;
         text-align: center;
         line-height: center;
     }
 
-    .el-button {
+    .el-button,
+    .el-button-default,
+    .el-button-mini {
         width: 45px;
         height: 45px;
         margin-top: 8px;
     }
 
-    .bg-green-50 {
+
+    .bg-gray-100 {
         --tw-bg-opacity: 1;
-        background-color: rgba(236, 253, 245, var(--tw-bg-opacity));
+        background-color: rgba(243, 244, 246, var(--tw-bg-opacity));
+    }
+
+    .bg-gray-400 {
+        --tw-bg-opacity: 1;
+        background-color: rgba(156, 163, 175, var(--tw-bg-opacity));
     }
 
     .black-font {
         color: #606266
     }
+
+    .left-door {
+        margin-left: v-bind(doorGap);
+    }
+
+    .right-door {
+        margin-right: v-bind(doorGap);
+    }
+
+    .white {
+        background-color: white;
+        color: #606266
+    }
+
+    .indicateLight {}
 </style>
